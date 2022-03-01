322773b3f57bb5f74a6d08701b28d31477f7d7df
lixinjian <lixinjian1217@gmail.com>
2022-03-01 15:21:40 -0500

    fix repo url path

--- mkbuild/config.py
+++ mkbuild/config.py
@@ -5,4 +5,4 @@ class MkbuildConfig:
     root_path = Path(__file__).parent.parent
     data_path = root_path / 'data'
     docs_path = root_path / 'docs'
-    github_root = "https://github.com/xinjli/cmu_multilingual_speech/tree/main"
+    github_root = "https://github.com/shinjiwlab/cmu_multilingual_speech/tree/main"
